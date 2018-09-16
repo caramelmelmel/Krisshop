d8aa667bed9cd61a434ae9523a16105503964b9c
Rayson Lim Jun Kai <rayson@jublia.com>
2018-09-16 22:20:08 +0800

    got permissions to prompt

--- app/src/main/java/ai/rt5k/krisshop/CustomerRecieveActivity.java
+++ app/src/main/java/ai/rt5k/krisshop/CustomerRecieveActivity.java
@@ -1,19 +1,67 @@
 package ai.rt5k.krisshop;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.PointF;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
 
 public class CustomerRecieveActivity extends AppCompatActivity {
 
-    private TextView resultTextView;
-    private QRCodeReaderView qrCodeReaderView;
+    private QRCodeReaderView qrcodeReaderView;
+    private static final int PERMISSIONS_REQUEST_CAMERA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_scanner);
+
+        // Check for camera permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    PERMISSIONS_REQUEST_CAMERA);
+
+        } else {
+            setQRReader();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted
+                    setQRReader();
+
+                } else {
+                    // permission denied
+
+                }
+                return;
+            }
+        }
+    }
+
+    private void setQRReader() {
+        qrcodeReaderView = findViewById(R.id.qrDecoderView);
+        qrcodeReaderView.setOnQRCodeReadListener(new QRCodeReaderView.OnQRCodeReadListener() {
+            @Override
+            public void onQRCodeRead(String text, PointF[] points) {
+            }
+        });
+        qrcodeReaderView.setQRDecodingEnabled(true);
+        qrcodeReaderView.setBackCamera();
     }
 }
+
+
