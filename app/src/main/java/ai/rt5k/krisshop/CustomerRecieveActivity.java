217e9c0db8bbf97176de31e987b284e22572a5c5
Rayson Lim Jun Kai <rayson@jublia.com>
2018-09-16 21:51:01 +0800

    add qrcode reader

---
+++ app/src/main/java/ai/rt5k/krisshop/CustomerRecieveActivity.java
@@ -0,0 +1,19 @@
+package ai.rt5k.krisshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+public class CustomerRecieveActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener {
+
+    private TextView resultTextView;
+    private QRCodeReaderView qrCodeReaderView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scanner);
+    }
+}
