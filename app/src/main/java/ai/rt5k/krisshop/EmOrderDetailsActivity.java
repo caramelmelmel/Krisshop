eb9156049ae881c1c8bb129b80799e4e4160b8cf
Rayson Lim Jun Kai <rayson@jublia.com>
2018-09-21 18:46:46 +0800

    Add layout for employee view

---
+++ app/src/main/java/ai/rt5k/krisshop/EmOrderDetailsActivity.java
@@ -0,0 +1,13 @@
+package ai.rt5k.krisshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmOrderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_em_order_details);
+    }
+}
